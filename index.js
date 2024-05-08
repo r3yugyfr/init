595e43684ed8139a765908587cca430c57351351
r3yugyfr <dsjcanielchacez@hotmail.com>
2024-05-08 11:27:21 +0700

    demo task commit

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function greet(name) {
-  console.log("Hello, " + name + "!");
+function binarySearch(arr, target) {
+  let low = 0;
+  let high = arr.length - 1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] === target) return mid;
+    if (arr[mid] < target) low = mid + 1;
+    else high = mid - 1;
+  }
+  return -1;
 }
-
-greet("Bob");
