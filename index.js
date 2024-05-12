864328a261805d5378b0bdafc985c8cdac056040
r3yugyfr <dsjcanielchacez@hotmail.com>
2024-05-13 06:49:07 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-function minimumTotal(triangle) {
-  const n = triangle.length;
-  const dp = new Array(n + 1).fill(0);
-  for (let i = n - 1; i >= 0; i--) {
-    for (let j = 0; j <= i; j++) {
-      dp[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);
-    }
-  }
-  return dp[0];
+function findNumbers(nums) {
+  return nums.filter((num) => num.toString().length % 2 === 0).length;
 }
