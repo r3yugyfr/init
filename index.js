8f25e3e2e7f55d6496bd94b5c32a1cd9575d199b
r3yugyfr <dsjcanielchacez@hotmail.com>
2024-05-13 20:56:42 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function findNumbers(nums) {
-  return nums.filter((num) => num.toString().length % 2 === 0).length;
+function removeNthFromEnd(head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
+  for (let i = 0; i <= n; i++) {
+    first = first.next;
+  }
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
+  }
+  second.next = second.next.next;
+  return dummy.next;
 }
