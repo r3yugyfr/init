a6cabc4053d4fc2723e87b15c159455d5353eeb6
r3yugyfr <dsjcanielchacez@hotmail.com>
2024-05-16 18:10:55 +0700

    update

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-function deserialize(data) {
-  if (!data.length) return null;
-  const root = new TreeNode(data.shift());
-  const queue = [root];
-  while (queue.length) {
-    const node = queue.shift();
-    const leftVal = data.shift();
-    if (leftVal !== undefined) {
-      node.left = new TreeNode(leftVal);
-      queue.push(node.left);
-    }
-    const rightVal = data.shift();
-    if (rightVal !== undefined) {
-      node.right = new TreeNode(rightVal);
-      queue.push(node.right);
-    }
+const fibonacciRecursive = (n) => {
+  if (n <= 1) {
+    return n;
   }
-  return root;
-}
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+};
